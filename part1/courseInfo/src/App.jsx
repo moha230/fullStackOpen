6c0791676644d5668c869a50c5d6e0ce01438d72
moha230 <117535847+moha230@users.noreply.github.com>
2025-07-11 03:36:03 +0300

    Done with 1.3: Course Information step 3

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,14 +1,9 @@
-
-
-
-
 //Create Header component that takes in an argument named props
 //props is an object that contain all data passed into the component
 const Header = (props) => {
   return (
     /*JSX creating a div and h1 tag and insert js values inside JSX*/
     <div>
-      
       <h1>{props.course}</h1>
     </div>
   );
@@ -21,54 +16,62 @@ const Part = (props) => {
     <p>
       {props.name} {props.exercises}
     </p>
-  )
-}
+  );
+};
 
 //Create a Content component that takes in an argument named props
 const Content = (props) => {
   return (
     <div>
-     <Part name={props.part1} exercises={props.exercises1}/>
-     <Part name={props.part2} exercises={props.exercises2}/>
-     <Part name={props.part3} exercises={props.exercises3}/>
+      <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
     </div>
   );
 };
-
-
 
 //Create a Total component that takes in an argument named props
 
 const Total = (props) => {
   return (
-     <div> <p>Number of exercises {props.total}</p></div>
-  )
-}
+    <div>
+      {" "}
+      <p>Number of exercises {props.total}</p>
+    </div>
+  );
+};
 
 const App = () => {
+  //Make the variables into objects
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
-      {/*
-      the course name is the variable name 
-      we assign it a value which is course which is defined on top with data 
-      we insert the variable in {} 
-      pass the course variable name into the component */}
-
       <Header course={course} />
-      <Content 
-      part1= {part1} exercises1={exercises1}
-      part2= {part2} exercises2= {exercises2}
-      part3= {part3} exercises3 = {exercises3}
+      <Content
+        part1={part1.name}
+        exercises1 = {part1.exercises}
+        
+        part2={part2.name}
+        exercises2 = {part2.exercises}
+        
+        part3={part3.name}
+        exercises3 = {part3.exercises}
+        
       />
-      <Total  total = {exercises1 + exercises2 + exercises3}/>
+      <Total total = {part1.exercises+ part2.exercises + part3.exercises} />
     </div>
   );
 };
