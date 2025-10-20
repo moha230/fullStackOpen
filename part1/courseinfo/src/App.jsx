988b0bd44a82cdd5a92c608e581043afb5c1c7e3
moha230 <117535847+moha230@users.noreply.github.com>
2025-10-20 17:44:57 +0300

    chore:created a header component and its working

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,61 +1,41 @@
 const App = () => {
-  const course = "Half Stack application development"; //header section
+  //Let's take the changes one step further. Change the course and its parts into a single JavaScript object. Fix everything that breaks
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
 
-  //content data  //changing it into an object and lets place the object into an array removed const= part1 part2 and part3
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
 
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts= {parts} />
     </div>
   );
 };
 
-//Define Header component and introduce props with the property name course
-//The component receives the props (properties) from its parent App component
-// and uses the props.course to display the course name.
-
+//create a component for the header
 const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  );
+  return <div>
+    <h1>{props.course.name}</h1>
+  </div>;
 };
 
 //create a component for content
 
-const Content = (props) => {
-  return <div>
-    <p>{props.parts[0].name} {props.parts[0].exercises}</p>
-    <p>{props.parts[1].name} {props.parts[1].exercises}</p>
-    <p>{props.parts[2].name} {props.parts[2].exercises}</p>
-  </div>;
-};
-
-//create a component for Total 
-
-const Total = (props) => {
-  return (<div>
-    <p>Total number of exercises {props.parts[0].exercises + props.parts[1].exercises+props.parts[2].exercises}</p>
-  </div>)
-}
+//create a component for Total
 
 export default App;
