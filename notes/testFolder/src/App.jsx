aabbfe8c17ddf00c34c5651b94f245da6f34fa44
moha230 <117535847+moha230@users.noreply.github.com>
2025-12-15 11:54:02 +0200

    reading: done with page rendering

--- notes/testFolder/src/App.jsx
+++ notes/testFolder/src/App.jsx
@@ -1,36 +1,23 @@
 
-//create a component that shows this data
 
-const Hello = ({name, age}) => {
-  //destructring the props object 
+
+
+//helper function
+
+const App = (props) => {
+ 
   
   return (
     <div>
-      <p>
-        Hello {name} your age is  {age}
-      </p>
+      {props.counter}
     </div>
   )
 }
 
+export default App
 
 
 
 
 
 
-const App = () => {
-  //variables of data 
-  const name = "peter";
-  const age = 10;
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age}/>
-      
-    </div>
-  )
-};
-
-export default App;
