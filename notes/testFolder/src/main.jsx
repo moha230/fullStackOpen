8d7988f80a7804d9c469c8f8f2d9b554830d0f97
moha230 <117535847+moha230@users.noreply.github.com>
2026-04-18 12:37:51 +0300

    reading: done with page rendering

--- notes/testFolder/src/main.jsx
+++ notes/testFolder/src/main.jsx
@@ -1,5 +1,22 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 
-import App from './App'
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+let counter = 1;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// create a function that forces rendering of the page
+
+const refresh = () => {
+  root.render(<App counter={counter} />);
+};
+
+
+//call the function using set intervals 
+
+setInterval(()=> {
+  refresh()
+  counter +=1
+}, 1000)
+
