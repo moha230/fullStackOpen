ec6a44d3b55af7a25e1d74876b2a793e4cfbb855
moha230 <117535847+moha230@users.noreply.github.com>
2026-04-18 12:37:50 +0300

    feat:Total component done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -11,16 +11,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <p>
-      <Content  part1={part1} exercises1={exercises1}/>
-      </p>
-      <p>
-      <Content  part2={part2} exercises2={exercises2}/>
-      </p>
-      <p>
-       <Content  part3={part3} exercises3={exercises3}/>
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content part1={part1} exercises1={exercises1} />
+      <Content part2={part2} exercises2={exercises2} />
+      <Content part3={part3} exercises3={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
@@ -43,10 +37,26 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
     </>
+  );
+};
+
+// Define Total component
+
+const Total = (props) => {
+  return (
+    <div>
+      <p> Number of exercises {props.total}</p>
+    </div>
   );
 };
 
