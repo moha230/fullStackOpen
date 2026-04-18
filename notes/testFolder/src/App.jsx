b391d94564747c1ed00d78953c087fc32b2d62b2
moha230 <117535847+moha230@users.noreply.github.com>
2026-04-18 12:37:51 +0300

    reading : done with an event handler is a function

--- notes/testFolder/src/App.jsx
+++ notes/testFolder/src/App.jsx
@@ -1,25 +1,30 @@
-import {useState} from 'react';// import the use State function 
+import { useState } from "react"; // import the use State function
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
 
+  // separting the event handlers in seprate functions
 
-  setTimeout(
-    () => 
-    setCounter(counter - 1),1000
-)
+  //increment function
+  const incrementByOne = () => setCounter(counter + 1);
 
-console.log('rendering...', counter)
+  //decrement function
+  const decrementByOne = () => setCounter(counter - 1);
 
+  //reset function
+
+  const resetCounter = () => setCounter(0);
   return (
-    <div>{counter}</div>
-  )
-
-}
-
+    <div>
+      <div>{counter}</div>
+      <hr />
+      <button onClick={incrementByOne}>plus</button>
+      <hr />
+      <button onClick={decrementByOne}>minus</button>
+      <hr />
+      <button onClick={resetCounter}>reset</button>
+    </div>
+  );
+};
 
 export default App;
-
-
-
-
